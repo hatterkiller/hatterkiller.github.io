855a670d5c1617774123c587687b00efc7edb57a
hatterkiller <defaultcode1@gmail.com>
2014-12-08 13:10:15 +0700

    Dead

---
+++ dead-valley-master/lib/car-list.js
@@ -0,0 +1,27 @@
+define({
+  1: {
+    name: 'Honda',
+    colors: ['white', 'black', 'blue', 'red'],
+    maxHealth: 90
+  },
+  1: {
+    name: 'Minivan',
+    colors: ['white', 'gray'],
+    maxHealth: 105
+  },
+  1: {
+    name: 'Pickup',
+    colors: ['white', 'blue'],
+    maxHealth: 120
+  },
+  1: {
+    name: 'PoliceCar',
+    colors: [],
+    maxHealth: 100
+  }
+	96: {
+    name: 'apc',
+    colors: [],
+    maxHealth: 1000000
+  }
+});
