63b06091e0ee477c23cd42feac77337ff82d0d67
Daiki <defaultcode1@gmail.com>
2015-07-06 18:25:35 +0700

    Ay

--- js.js
+++ js.js
@@ -1,4 +1,15 @@
 var main = function() {
+ /*   $('.menuclicky').hover(function() 
+{     
+
+    $('.menuclicky').css("background-color", "#484A47"); 
+}, function(){
+        // change to any color that was previously used.
+                 $('.menuclicky').css("background-color", "transparent"); 
+
+    });*/
+    
+    
   $('.menuclicky').click(function() {
     $('.menu').animate({
       left: "0px"
@@ -7,8 +18,9 @@ var main = function() {
     $('body').animate({
       left: "285px"
     }, 200);
-        });
+  });
 
+    
  $('.button-close').click(function () {
     $('.menu').animate({
                 left: "-285px"
@@ -17,19 +29,26 @@ var main = function() {
     $('body').animate({
       left: "0px"
     }, 200);
+
   });
 
 };
-var hide = function() {
+
+
+
+
+/*var hide = function() {
 $('.lol').hover(function() 
-{ 
+{     
+
     $('.mainbody').css("background-image", "url(walrus.jpg)"); 
+
 }, function(){
         // change to any color that was previously used.
         $('.mainbody').css('background-image', 'none');
     });
 };
-
+*/
 
 
 
