ae29b640cbc3c5cf4e5683bcadd21d660bec48b8
Daiki <defaultcode1@gmail.com>
2015-10-24 09:00:30 +0700

    Fix Laornual

--- counter.php
+++
@@ -1,5 +0,0 @@
-
-<!-- saved from url=(0038)http://csgo-duel.com/click/counter.php -->
-<html><head><meta http-equiv="Content-Type" content="text/html; charset=windows-1252"></head><body><span class="hour">0 : </span>
-<span class="minutes">0 : </span>
-<span class="secondes">10 <p style="font-size:15px;display:inline-block"> before next click</p></span></body></html>
