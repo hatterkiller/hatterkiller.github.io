92e16ee001384f643ec95738f2fcea213477d23d
Daiki <defaultcode1@gmail.com>
2015-11-19 21:59:38 +0700

    StucoWebsite v1.2
    Added Student Issues
    Changed menu bar
    Drop Down for Documents

--- scwebsite/js.js
+++ scwebsite/js.js
@@ -32,7 +32,30 @@ var main = function() {
 
   });
 
-};
+    
+    // create the back to top button
+
+
+$('body').prepend('<a href="#" class="back-to-top">Back to Top</a>');
+
+var amountScrolled = 300;
+
+$(window).scroll(function() {
+	if ($(window).scrollTop() > amountScrolled) {
+		$('a.back-to-top').fadeIn('slow');
+	} else {
+		$('a.back-to-top').fadeOut('slow');
+	}
+});
+
+$('a.back-to-top, a.simple-back-to-top').click(function() {
+	$('body').animate({
+		scrollTop: 0
+	}, 'fast');
+	return false;
+});
+
+};/*End of Main*/
 
 
 
